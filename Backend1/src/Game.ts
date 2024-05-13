4b3eab45bd298eae8650bab98d18f98833d53409
ADITI AGARWAL <45040406+aditigoyal1@users.noreply.github.com>
2024-05-13 16:51:17 +0530

    updated

---
+++ Backend1/src/Game.ts
@@ -0,0 +1,126 @@
+import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, INIT_GAME, MOVE ,WRONG_PLAYER_MOVE} from "./message";
+
+export class Game {
+  public player1: WebSocket;
+  public player2: WebSocket;
+  private board: Chess;
+  private startTime: Date;
+  private moveCount;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.startTime = new Date();
+    this.moveCount =0;
+
+    // whenever the game start, we need to let them know tht the game has started with their color.
+
+
+    try {
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload:{
+                color:"white"
+            }
+        }));
+    
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload:{
+                color:"black"
+            }
+        }));
+        
+    } catch (error) {
+        console.log("Errooor==>", error)
+        return
+        
+    }
+   
+  }
+
+  makeMove(
+    socket: WebSocket,
+    move: {
+      from: string;
+      to: string;
+    }
+  ) {
+    // validation here whether the correct player moving or not.
+    if (this.moveCount % 2 === 0 && this.player1 != socket) {
+        this.player2.send(
+            JSON.stringify({
+              type: WRONG_PLAYER_MOVE,
+              message: "It's not your turn!!"
+            })
+          );
+      return;
+    }
+
+    if (this.moveCount % 2 === 1 && this.player2 != socket) {
+        this.player1.send(
+            JSON.stringify({
+              type: WRONG_PLAYER_MOVE,
+              message: "It's not your turn!!"
+            })
+          );
+      return;
+    }
+
+    // Is this users move valid or not
+    ////Update the board
+    try {
+      this.board.move(move);
+      this.moveCount++;
+    } catch (error) {
+        console.error(error);
+        return
+    }
+
+    // check if the game is over or not
+    if (this.board.isGameOver()) {
+      this.player1.send(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+
+      this.player2.send(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      return;
+    }
+
+    //emit the move and moveCount already increased
+    if (this.moveCount % 2 === 0) {
+      this.player1.send(
+        JSON.stringify({
+          type: MOVE,
+          move: move,
+        })
+      );
+      return
+    } else {
+      this.player2.send(
+        JSON.stringify({
+          type: MOVE,
+          move: move,
+        })
+      );
+      return
+    }
+
+    
+  }
+}
---
+++ Backend1/src/GameManager.ts
@@ -0,0 +1,59 @@
+import { Game } from "./Game";
+import { WebSocket } from "ws";
+import { INIT_GAME, MOVE } from "./message";
+
+
+export class GameManager {
+    private games: Game[];
+    private pendingGameUser: WebSocket | null;
+    private users: WebSocket[];
+
+    constructor(){
+        this.games = []
+        this.users = []
+        this.pendingGameUser = null
+    }
+
+    addUser(socket: WebSocket){
+        this.users.push(socket);
+        this.addHandler(socket)
+        
+    }
+
+    removeUser(socket:WebSocket){
+        this.users= this.users.filter(user=> user!=socket);
+        // TODO stop the game here as the user left.
+    }
+
+    private addHandler(socket: WebSocket){
+        socket.on("message", (data)=> {
+            const message = JSON.parse(data.toString());
+
+
+            if(message.type == INIT_GAME){
+                if(this.pendingGameUser){
+                    // start a game directly as we have two players
+                    const game = new Game(this.pendingGameUser, socket);
+                    this.games.push(game);
+                    this.pendingGameUser = null
+                    console.log("Game started");
+                }else {
+                    console.log("Waiting for another user")
+                    this.pendingGameUser=socket;
+                }
+            }
+
+            if(message.type == MOVE){
+                const game = this.games.find(game=>game.player1==socket || game.player2==socket)
+                if(game){
+                    game.makeMove(socket,message.move);
+                }
+
+            }
+
+        })
+
+
+    }
+
+}
